ca12bf17cf1b3092acfa98c649b4f6f2ff599c1a
Mafi-Git <2108027@edu.karelia.fi>
2022-12-07 16:20:54 +0200

    eka committi

---
+++ src/Etusivu.js
@@ -0,0 +1,18 @@
+const Etusivu = () => {
+    return (
+        <div className="Etusivu">
+            <h1 className="eraopaslogo">Eräopas</h1>
+            <div className="navigointi">
+                <a href="">Etusivu </a>
+                <a href="">Eräopas </a>
+                <a href="">Ota yhteyttä </a>
+                <a href="">Tuotteet </a>
+                <a href="">about </a>
+            </div>
+        </div>
+    );
+}
+ 
+export default Etusivu;
+
+
